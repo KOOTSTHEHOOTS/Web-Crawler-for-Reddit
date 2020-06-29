a94129b2a4592cc0e21d241ef4cced7fe9b28646
KOOTSTHEHOOTS <x>
2020-06-29 21:40:00 +0800

    Read text file containing subreddits to scrape

--- Title Scrapper.py
+++ Title Scrapper.py
@@ -1,5 +1,15 @@
 import praw
 
+#subreddits to crawl
+subreddits=[]
+
+with open("Subreddits.txt", "r") as f:
+    # add all subreddits from text file to filter
+    for subreddit in f:
+        subreddits.append(subreddit.strip())
+        
+print(subreddits)
 # Create read only instance of Reddit
 # Bot1 settings is selected from praw.ini
 reddit = praw.Reddit("Bot1", config_interpolation="basic")
+
