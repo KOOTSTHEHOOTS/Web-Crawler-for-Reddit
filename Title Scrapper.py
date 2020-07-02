897353eae2e49fa9754316a93a2f6fb2526e257b
KOOTSTHEHOOTS <x>
2020-07-02 17:06:57 +0800

    Change search method to use regex
    - use regex to search for keywords rather than the "in" which matched substrings

--- Title Scrapper.py
+++ Title Scrapper.py
@@ -1,9 +1,10 @@
 import praw
 import time
+import re
 
 #subreddits to crawl
 subreddits=[]
-keywords=[]
+keywords_list=[]
 with open("Subreddits.txt", "r") as f:
     # add all subreddits from text file to filter
     for subreddit in f:
@@ -12,51 +13,52 @@ with open("Subreddits.txt", "r") as f:
 with open("Keyword.txt", "r") as f:
     # add all subreddits from text file to filter
     for keyword in f:
-        keywords.append(keyword.strip().lower())
+        keywords_list.append(keyword.strip().lower())
 print(subreddits)
-print(keywords)
+print(keywords_list)
+
 # Create read only instance of Reddit
 # Bot1 settings is selected from praw.ini
 reddit = praw.Reddit("Bot1", config_interpolation="basic")
-
-        
 subreddit_name="+".join(subreddits)
 print(subreddit_name)
 
 subreddit=reddit.subreddit(subreddit_name)
+keywords=re.compile(rf"\b{'|'.join(keywords_list)}\b", re.IGNORECASE)
+
 counter=0
 for submission in subreddit.stream.submissions():
-    for keyword in keywords:
-        if submission==None:
-            # print(None)
-            # time.sleep(1)
-            break
-            
-        elif keyword in submission.title:
-            print(f"{keyword}\n\n{submission.title}")
-            time.sleep(1)
-            break
-       
-        else:
-            # # print("Keyword Not Found")
-            # time.sleep(1)
-            break
+    if submission==None:
+        # print(None)
+        # time.sleep(1)
+        break
+    
+    found=keywords.search(submission.title)
+    
+    if found!=None:
+        print(found.expand(fr"Matched on {found.group(0)} \n{submission.title}"))
+        time.sleep(1)
+   
+    else:
+        pass
+        # # print("Keyword Not Found")
+        # time.sleep(1)
+
     counter+=1
     print(counter)
 
-for comment in subreddit.stream.comments():
-    for keyword in keywords:
-        if comment==None:
-            print(None)
-            time.sleep(1)
-            break
+# for comment in subreddit.stream.comments():
+#         if comment==None:
+#             print(None)
+#             time.sleep(1)
+#             break
             
-        elif keyword in comment.body:
-            print(f"{keyword}\n\n{comment.body}")
-            time.sleep(1)
-            break
+#         elif keyword in comment.body:
+#             print(f"{keyword}\n\n{comment.body}")
+#             time.sleep(1)
+#             break
        
-        else:
-            print("Keyword Not Found")
-            time.sleep(1)
-            break
+#         else:
+#             print("Keyword Not Found")
+#             time.sleep(1)
+#             break
